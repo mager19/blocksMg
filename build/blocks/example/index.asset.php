861272a0a708ab0ab0dff6b16bf14fe0c63fb804
Mario Germán Reyes C <mager19@gmail.com>
2023-12-27 16:02:40 -0500

    progress section header - editor ready

--- build/blocks/example/index.asset.php
+++ build/blocks/example/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'dcff9f9ee4faf1f8c95d');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'a8adb4285c4c3775931c');
---
+++ build/blocks/section-header/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '346672d0042ee7afea68');
